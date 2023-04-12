8218a28f3270fcc0008ddc406ac637da63752d75
Henrique Kriguer <henriquekriguer@gmail.com>
2023-04-12 20:06:28 -0300

    separando o main.js em dois arquivos

--- AULA_06_iNTRODUCAO_DEV_WEB/assets/js/main.js
+++ AULA_06_iNTRODUCAO_DEV_WEB/assets/js/main.js
@@ -1,10 +1,10 @@
 
 const offset = 0;
-const limit = 10;
+const limit = 100;
 const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`;
 
-function convertPokemonToLi(pokemon){
-    return`
+function convertPokemonToLi(pokemon) {
+    return `
         <li class="pokemon">
             <span class="number">#001</span>
             <span class="name">${pokemon.name}</span>
@@ -23,17 +23,12 @@ function convertPokemonToLi(pokemon){
 }
 const pokemonList = document.getElementById('pokemonList')
 
-
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => jsonBody.results) // adiciona este passo para retornar a lista de pokemons
-    .then((pokemons)=>{
-
-        for (let i = 0; i < pokemons.length; i++) {
-            const pokemon = pokemons[i];
-            pokemonList.innerHTML += convertPokemonToLi(pokemon)
-        }
-    })
+pokeApi.getPokemons().then((pokemons) => {
+    for (let i = 0; i < pokemons.length; i++) {
+        const pokemon = pokemons[i];
+        pokemonList.innerHTML += convertPokemonToLi(pokemon)
+    }
+})
     .catch((error) => console.error(error))
-    
+
 
