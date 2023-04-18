1e72e7911bc4cb1cad962400678f3dc90615ae1b
Henrique Kriguer <henriquekriguer@gmail.com>
2023-04-18 16:14:20 -0300

    Convert pokeAPI model to our model to simplify the path for identify the pokemon parameters

--- AULA_06_iNTRODUCAO_DEV_WEB/assets/js/poke-api.js
+++ AULA_06_iNTRODUCAO_DEV_WEB/assets/js/poke-api.js
@@ -1,11 +1,35 @@
-const offset = 0;
-const limit = 100;
-const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`;
 
 const pokeApi = {}
+
+function convertPokeApiDetailToPokemon(pokeDetail){
+    const pokemon = new Pokemon();
+    pokemon.name = pokeDetail.name
+    pokemon.number = pokeDetail.order
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+
+    pokemon.types = types
+    pokemon.type = type
+
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+
+    return pokemon
+}
+
+pokeApi.getPokemoDetail = (pokemon) => {
+    return fetch(pokemon.url)
+        .then((response) => response.json())
+        .then(convertPokeApiDetailToPokemon) 
+}
+
 pokeApi.getPokemons = (offset = 0, limit = 10) => {
+    const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`;
+
     return fetch(url)
         .then((response) => response.json())
         .then((jsonBody) => jsonBody.results)
+        .then((pokemons) => pokemons.map(pokeApi.getPokemoDetail))
+        .then((detailRequest) => Promise.all(detailRequest))
+        .then((pokemonsDetail) => pokemonsDetail)
         .catch((error) => console.error(error))
 }
