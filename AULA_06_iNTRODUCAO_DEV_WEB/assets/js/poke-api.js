de73cdfd626dab59a4afef3c2c0872213092b540
Henrique Kriguer <henriquekriguer@gmail.com>
2023-04-18 20:45:36 -0300

    Adding pagination in pokemons document

--- AULA_06_iNTRODUCAO_DEV_WEB/assets/js/poke-api.js
+++ AULA_06_iNTRODUCAO_DEV_WEB/assets/js/poke-api.js
@@ -4,7 +4,7 @@ const pokeApi = {}
 function convertPokeApiDetailToPokemon(pokeDetail){
     const pokemon = new Pokemon();
     pokemon.name = pokeDetail.name
-    pokemon.number = pokeDetail.order
+    pokemon.number = pokeDetail.id
     const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
     const [type] = types
 
@@ -22,7 +22,7 @@ pokeApi.getPokemoDetail = (pokemon) => {
         .then(convertPokeApiDetailToPokemon) 
 }
 
-pokeApi.getPokemons = (offset = 0, limit = 20) => {
+pokeApi.getPokemons = (offset = 0,limit=10) => {
     const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`;
 
     return fetch(url)
